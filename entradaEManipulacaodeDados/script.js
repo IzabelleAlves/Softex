d56d6ab6468dafe20f7eb65db047a542c639a4ff
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-07-10 21:17:50 -0300

    Finalização da ficha

--- entradaEManipulacaodeDados/script.js
+++ entradaEManipulacaodeDados/script.js
@@ -7,36 +7,47 @@ console.log("O tipo de dado \u00E9: ".concat(typeof idade));
 //questao7
 console.log("Seu n\u00FAmero \u00E9 ".concat(idade, " e ele convertido para n\u00FAmero com casa decimal \u00E9 ").concat(idade.toFixed(2)));
 //questao8
-var num1 = Number(prompt("Digite um número: "));
-var num2 = Number(prompt("Digite outro número: "));
+var num1 = Number(prompt("Digite um número para realizar uma soma: "));
+var num2 = Number(prompt("Digite outro número para realizar a soma: "));
 console.log("A soma dos dois n\u00FAmeros digitados \u00E9 ".concat(num1 + num2));
 //questao9
-var numeroDecimal = Number(prompt("Digite um número decimal: "));
+var numeroDecimal = Number(prompt("Digite um número decimal para descobrirmos o quadrado dele: "));
 console.log("O quadrado do n\u00FAmero que voc\u00EA digitou \u00E9 ".concat((numeroDecimal * numeroDecimal).toFixed(2)));
 // questao10
 var nascimento = Number(prompt("Digite o ano em que você nasceu: "));
-console.log("A sua idade no ano atual \u00E9 ".concat(2024 - nascimento));
+console.log("No ano atual a sua idade \u00E9 de ".concat(2024 - nascimento, " anos"));
 //questao11
 var sobrenome = prompt("".concat(nome, ", digite seu sobrenome: "));
 console.log("Seu nome completo \u00E9 ".concat(nome, " ").concat(sobrenome));
 //questao12
 var numeros = prompt("Digite uma sequência de números separados por espaço: ");
-var array = numeros.split(" "); //split pega um conjunto de valores com um tipo de separador em específico (nesse caso é o espaço) e os transforma num array.
-console.log(array.length);
+var array = [];
+if (numeros != null) { // TRIM -> : Verifica se o campo está vazio ou apenas com espaços
+    array = numeros.split(" "); // split pega um conjunto de valores com um tipo de separador em específico (nesse caso é o espaço) e os transforma num array.
+}
+else {
+    console.log("Nenhum número foi digitado.");
+}
+console.log("A quantidade de caracteres da frase digitada \u00E9 de: ".concat(array.length));
 //questao13
 var animal = prompt("Digite o nome de um animal: ");
-console.log(animal);
+console.log("O nome do animal digitado foi: ".concat(animal));
 //questao14
 console.log("Seu nome na ordem inversa: ".concat(sobrenome, " ").concat(nome));
 //questao15
 var texto = prompt("Digite uma frase qualquer: ");
-var tamanhoTexto = texto;
-console.log(tamanhoTexto.length);
+if (texto != null) {
+    var tamanhoTexto = texto.length;
+    console.log(tamanhoTexto);
+}
+else {
+    console.log("Nenhuma frase foi digitada.");
+}
 //questao16
-var num = Number(prompt("Digite um número: "));
-console.log("".concat(num % 2 == 0 ? 'é par' : "é ímpar"));
+var num = Number(prompt("Digite um número para checarmos se ele é par ou ímpar, positivo ou negativo: "));
+console.log("".concat(num % 2 == 0 ? 'O número é par' : "O número é ímpar"));
 //questao17
-console.log("".concat(num >= 0 ? 'é positivo' : "é negativo"));
+console.log("".concat(num >= 0 ? 'O número é positivo' : "O número é negativo"));
 //questao18
 console.log("O maior n\u00FAmero que voc\u00EA digitou foi ".concat(Math.max(num, num1, num2)));
 //questao19
@@ -45,20 +56,29 @@ var altura = Number(prompt("Digite sua altura: "));
 var imc = peso / (altura * altura);
 console.log("".concat(nome, ", o seu IMC \u00E9 ").concat(imc.toFixed(2)));
 // questao20
-console.log("".concat(nome.length > 5 ? "O seu nome tem mais de 5 letras" : "O seu nome tem menos de 5 letras"));
+if (nome != null) {
+    console.log("".concat(nome.length > 5 ? "O seu nome tem mais de 5 letras" : "O seu nome tem menos de 5 letras"));
+}
 //questao21
 var estadoCivil = prompt("Digite seu estado civil: ");
-console.log("".concat(estadoCivil == "solteiro" || estadoCivil == "solteira" ? "Voce é solteiro" : estadoCivil == "casado" || estadoCivil == "casada" ? "Você é casado" : "você não é nada"));
+console.log("".concat(estadoCivil == "solteiro" || estadoCivil == "solteira" ? "Voce é solteiro(a)" : estadoCivil == "casado" || estadoCivil == "casada" ? "Você é casado(a)" : "Você não é nem solteiro e nem casado."));
+//questao22
+var baseTriangulo = Number(prompt("Digite a base do triangulo: "));
+var alturaTriangulo = Number(prompt("Digite a altura do triangulo: "));
+var areaTriangulo = baseTriangulo * alturaTriangulo;
+console.log("A base do triangulo \u00E9 ".concat(areaTriangulo));
 //questao23
 var cidade = prompt("Digite o nome da cidade em que você mora: ");
-console.log("".concat(cidade.charAt(0) == "R" ? "A cidade digitada começa com a letra R" : "A cidade digitada não começa com a letra R"));
+if (cidade != null) {
+    console.log("".concat(cidade.charAt(0) == "S" ? "A cidade digitada começa com a letra S" : "A cidade digitada não começa com a letra S"));
+}
 //questao24
 var numeroDecimal1 = Number(prompt("Digite um número decimal: "));
 var numeroDecimal2 = Number(prompt("Digite outro número decimal: "));
-console.log("O resto da divis\u00E3o dos n\u00FAmeros que voc\u00EA digitou \u00E9: ".concat((numeroDecimal1 % numeroDecimal2).toFixed(2)));
+console.log("O resto da divis\u00E3o dos n\u00FAmeros decimais que voc\u00EA digitou \u00E9: ".concat((numeroDecimal1 % numeroDecimal2).toFixed(2)));
 //questao25
-var digito = Number(prompt("Digite um número decimal: "));
-console.log(Math.floor(digito));
+var digito = Number(prompt("Digite um número decimal para arrendondarmos: "));
+console.log("O n\u00FAmero ".concat(digito, " arrendondado \u00E9 ").concat(Math.floor(digito)));
 //questao26
 var numInt = Number(prompt("Digite um número inteiro"));
 var numIntAdicionado = numInt + 10;
@@ -66,12 +86,21 @@ var numString = numIntAdicionado.toString();
 console.log("".concat(numString, " \u00E9 do tipo ").concat(typeof numString));
 //question27
 var dataNascimento = prompt("Digite sua data de nascimento. Exemplo: 'dd/mm/aaaa': ");
-var dataSeparada = dataNascimento.split("/");
-var diaStr = dataSeparada[0];
-var mesStr = dataSeparada[1];
-var anoStr = dataSeparada[2];
-var dia = parseInt(diaStr);
-var mes = parseInt(mesStr);
-var ano = parseInt(anoStr);
+var dataSeparada = [];
+if (dataNascimento != null) {
+    dataSeparada = dataNascimento.split("/");
+}
+var dia = parseInt(dataSeparada[0]);
+var mes = parseInt(dataSeparada[1]);
+var ano = parseInt(dataSeparada[2]);
 console.log("Dia: ".concat(dia, ", M\u00EAs: ").concat(mes, ", Ano: ").concat(ano));
 //questao28
+var estado = prompt("Você já disse a cidade em que mora, agora diga qual o estado: ");
+console.log("Voc\u00EA mora em ".concat(cidade, ", ").concat(estado, "."));
+//questao29
+console.log("Bem-vindo ao nosso programa, nascido em ".concat(nascimento, "!"));
+//questao30
+var dados1 = Number(prompt("Digite um número inteiro: "));
+var dados2 = prompt("Digite uma string: ");
+console.log("Dados concatenados: ".concat(dados1 + " " + dados2));
+//questao33
--- entradaEManipulacaodeDados/script.ts
+++ entradaEManipulacaodeDados/script.ts
@@ -1,51 +1,56 @@
 //questão 5
-let nome = prompt("Digite seu nome: ")
-  console.log(`Bem vinda(o), ${nome}!!`)
+const idade = prompt("Digite sua idade: ")
+let idadeConvertida = 0
+
+if (idade != null){
+  idadeConvertida = parseInt(idade)
+  console.log(`O tipo de dado é: ${typeof idadeConvertida}`)
+}
 
 //questao6
-let idade = Number(prompt("Digite sua idade: "))
-console.log(`O tipo de dado é: ${typeof idade}`)
+const nome = prompt("Digite seu nome: ")
+  console.log(`Bem vinda(o), ${nome}!!`)
 
 //questao7
-console.log(`Seu número é ${idade} e ele convertido para número com casa decimal é ${idade.toFixed(2)}`)
+console.log(`Seu número é ${idade} e ele convertido para número com casa decimal é ${idadeConvertida.toFixed(2)}`)
 
 //questao8
-let num1 = Number(prompt("Digite um número para realizar uma soma: "))
-let num2 = Number(prompt("Digite outro número para realizar a soma: "))
+const num1 = Number(prompt("Digite um número para realizar uma soma: "))
+const num2 = Number(prompt("Digite outro número para realizar a soma: "))
 console.log(`A soma dos dois números digitados é ${ num1 + num2}`)
 
 //questao9
-let numeroDecimal =  Number(prompt("Digite um número decimal para descobrirmos o quadrado dele: "))
+const numeroDecimal =  Number(prompt("Digite um número decimal para descobrirmos o quadrado dele: "))
 console.log(`O quadrado do número que você digitou é ${(numeroDecimal * numeroDecimal).toFixed(2)}`)
 
 // questao10
-let nascimento = Number(prompt("Digite o ano em que você nasceu: "))
+const nascimento = Number(prompt("Digite o ano em que você nasceu: "))
 console.log(`No ano atual a sua idade é de ${2024 - nascimento} anos`)
 
 //questao11
-let sobrenome = prompt(`${nome}, digite seu sobrenome: `)
+const sobrenome = prompt(`${nome}, digite seu sobrenome: `)
 console.log(`Seu nome completo é ${nome} ${sobrenome}`)
 
 //questao12
-let numeros = prompt("Digite uma sequência de números separados por espaço: ")
-let array = [];
+const numeros = prompt("Digite uma sequência de números separados por espaço: ")
+let array: string[] = []
 
-if (numeros != null && numeros.trim() != "") { // TRIM -> : Verifica se o campo está vazio ou apenas com espaços
-  let array = numeros.split(" "); // split pega um conjunto de valores com um tipo de separador em específico (nesse caso é o espaço) e os transforma num array.
+if (numeros != null) { // TRIM -> : Verifica se o campo está vazio ou apenas com espaços
+  array = numeros.split(" "); // split pega um conjunto de valores com um tipo de separador em específico (nesse caso é o espaço) e os transforma num array.
 } else {
   console.log("Nenhum número foi digitado.");
 }
 console.log(`A quantidade de caracteres da frase digitada é de: ${array.length}`)
 
 //questao13
-let animal = prompt("Digite o nome de um animal: ")
+const animal = prompt("Digite o nome de um animal: ")
 console.log(`O nome do animal digitado foi: ${animal}`)
 
 //questao14
 console.log(`Seu nome na ordem inversa: ${sobrenome} ${nome}`)
 
 //questao15
-let texto = prompt("Digite uma frase qualquer: ")
+const texto = prompt("Digite uma frase qualquer: ")
 
 if (texto != null) {
   let tamanhoTexto = texto.length
@@ -55,8 +60,8 @@ if (texto != null) {
 }
 
 //questao16
-let num = Number(prompt("Digite um número para checarmos se ele é par ou ímpar, positivo ou negativo: "))
-console.log(`${num % 2 == 0 ? 'O número é par' : "O número éé ímpar"}`)
+const num = Number(prompt("Digite um número para checarmos se ele é par ou ímpar, positivo ou negativo: "))
+console.log(`${num % 2 == 0 ? 'O número é par' : "O número é ímpar"}`)
 
 //questao17
 console.log(`${num >= 0 ? 'O número é positivo' : "O número é negativo"}`)
@@ -65,8 +70,8 @@ console.log(`${num >= 0 ? 'O número é positivo' : "O número é negativo"}`)
 console.log(`O maior número que você digitou foi ${Math.max(num, num1, num2)}`)
 
 //questao19
-let peso = Number(prompt("Digite seu peso: "))
-let altura = Number(prompt("Digite sua altura: "))
+const peso = Number(prompt("Digite seu peso: "))
+const altura = Number(prompt("Digite sua altura: "))
 let imc = peso / (altura * altura)
 
 console.log(`${nome}, o seu IMC é ${imc.toFixed(2)}`)
@@ -77,49 +82,140 @@ if (nome != null){
 }
 
 //questao21
-let estadoCivil = prompt("Digite seu estado civil: ")
+const estadoCivil = prompt("Digite seu estado civil: ")
 
 console.log(`${estadoCivil == "solteiro" || estadoCivil == "solteira" ? "Voce é solteiro(a)" : estadoCivil == "casado" || estadoCivil == "casada" ? "Você é casado(a)" : "Você não é nem solteiro e nem casado."}`)
 
+//questao22
+
+const baseTriangulo = Number(prompt("Digite a base do triangulo: "))
+const alturaTriangulo = Number(prompt("Digite a altura do triangulo: "))
+const areaTriangulo = baseTriangulo * alturaTriangulo
+
+console.log(`A base do triângulo é ${areaTriangulo}`)
+
 //questao23
-let cidade = prompt("Digite o nome da cidade em que você mora: ")
+const cidade = prompt("Digite o nome da cidade em que você mora: ")
 if (cidade != null) {
-  console.log(`${cidade.charAt(0) == "R" ? "A cidade digitada começa com a letra R" : "A cidade digitada não começa com a letra R"}`);
+  console.log(`${cidade.charAt(0) == "S" ? "A cidade digitada começa com a letra S" : "A cidade digitada não começa com a letra S"}`);
 }
 
 //questao24
-let numeroDecimal1 = Number(prompt("Digite um número decimal: "))
-let numeroDecimal2 = Number(prompt("Digite outro número decimal: "))
+const numeroDecimal1 = Number(prompt("Digite um número decimal: "))
+const numeroDecimal2 = Number(prompt("Digite outro número decimal: "))
 
 console.log(`O resto da divisão dos números decimais que você digitou é: ${(numeroDecimal1 % numeroDecimal2).toFixed(2)}`)
 
 //questao25
-let digito = Number(prompt("Digite um número decimal para arrendondarmos: "))
+const digito = Number(prompt("Digite um número decimal para arrendondarmos: "))
 console.log(`O número ${digito} arrendondado é ${Math.floor(digito)}`)
 
 //questao26
-let numInt = Number(prompt("Digite um número inteiro"))
+const numInt = Number(prompt("Digite um número inteiro"))
 let numIntAdicionado = numInt + 10
 let numString = numIntAdicionado.toString()
 
 console.log(`${numString} é do tipo ${typeof numString}`)
 
 //question27
-let dataNascimento = prompt("Digite sua data de nascimento. Exemplo: 'dd/mm/aaaa': ")
+const dataNascimento = prompt("Digite sua data de nascimento. Exemplo: 'dd/mm/aaaa': ")
 let dataSeparada: string[] = []
 
 if (dataNascimento != null) {
   dataSeparada = dataNascimento.split("/")
 }
 
-let diaStr: string = dataSeparada[0]
-let mesStr: string = dataSeparada[1]
-let anoStr: string = dataSeparada[2]
-
-let dia: number = parseInt(diaStr)
-let mes: number = parseInt(mesStr)
-let ano: number = parseInt(anoStr)
+let dia: number = parseInt(dataSeparada[0])
+let mes: number = parseInt(dataSeparada[1])
+let ano: number = parseInt(dataSeparada[2])
 
 console.log(`Dia: ${dia}, Mês: ${mes}, Ano: ${ano}`)
 
 //questao28
+
+const estado = prompt("Você já disse a cidade em que mora, agora diga qual o estado: ")
+
+console.log(`Você mora em ${cidade}, ${estado}.`)
+
+//questao29
+console.log(`Bem-vindo ao nosso programa, nascido em ${nascimento}!`)
+
+//questao30
+
+const dados1 = Number(prompt("Digite um número inteiro: "))
+const dados2 = prompt("Digite uma string: ")
+
+console.log(`Dados concatenados: ${dados1 + " " + dados2}`)
+
+//questao31
+
+const produto = prompt("Digite o produto: ")
+const preco = prompt("Digite o preço do produto: ")
+
+console.log(`Resumo pedido: ${produto}, R$${preco}`)
+
+//questao32
+
+console.log(`O dobro de ${numInt} é igual a ${numInt ** 2}`)
+
+//questao33
+
+const email = prompt("Digite seu e-mail: ")
+console.log(`Obrigada por se inscrever. Continuaremos enviando notícias para o e-mail: ${email}!!`)
+
+//questao34
+
+console.log(`A soma de ${num} + ${numInt} é ${num + numInt}`)
+console.log(`A diferença de ${num} + ${numInt} é ${Math.abs(num - numInt)}`)
+console.log(`O produto de ${num} + ${numInt} é ${num * numInt}`)
+console.log(`O quociente de ${num} + ${numInt} é ${num / numInt}`)
+
+//questao35
+
+const baseTriangulo1 = Number(prompt("Digite a base do triangulo: "))
+const alturaTriangulo1 = Number(prompt("Digite a altura do triangulo: "))
+const areaTriangulo1 = baseTriangulo1 * alturaTriangulo1
+console.log(`A base do triângulo é ${areaTriangulo1}`)
+
+//questao36
+
+const raio = Number(prompt("Digite o raio de uma circunferência: "))
+const pi = 3.14
+const perimetro = 2 * pi * raio
+
+console.log(perimetro)
+
+//questao37
+
+console.log(`O perímetro do triângulo 1 é: ${2 * (baseTriangulo + alturaTriangulo)}`)
+
+//questao38
+
+console.log(`A média aritmética entre os três números decimais já digitados aqui é: ${(numeroDecimal + numeroDecimal1 + numeroDecimal2 / 3)}`)
+
+//questao39
+
+console.log(`${nome}, você já viveu ${idadeConvertida * 365} dias e ${idadeConvertida * 12} meses`)
+
+//questao40
+
+const valorReal = Number(prompt("Digite o valor valor em real: "))
+const cotacaoDolar = Number(prompt("Digite a cotação do dólar atualmente: "))
+const valorDolar = valorReal * cotacaoDolar
+
+console.log(valorDolar)
+
+// questao41 
+
+console.log(`O número decimal ${numeroDecimal}convertido para o número mais próximo é: ${Math.ceil(numeroDecimal)}`)
+
+// questao42
+
+console.log(`O resultado da operação com três números inteiros ((n1 + n2) * n3) é: ${(num + num1) * numInt}`)
+
+// questao43
+
+const temperatura = Number(prompt("Digite uma temperatura em graus Celsius: "))
+const temperaturaConvertida = (temperatura * 9/5) + 32
+
+console.log(temperaturaConvertida)
