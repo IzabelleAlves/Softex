faa8296e348750e0549add04ff667363081951f0
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-10-16 16:51:02 -0300

    try e catch

--- componentes-react/get-adress.ts
+++ componentes-react/get-adress.ts
@@ -1,11 +1,17 @@
 async function getAddress(cep: string){ // async deixa a função assíncrona
-    const url = `https://viacep.com.br/ws/${cep}/json/`
-    console.log(url)
 
-    const response = await fetch(url) // await diz que vai puxar a resposta do servidor de forma assincrona
-    const data = await response.json()
-    console.log(data)
-    //procesamento
+    try {
+        const url = `https://viacep.com/ws/${cep}/json/`
+        console.log(url)
+    
+        const response = await fetch(url) // await diz que vai puxar a resposta do servidor de forma assincrona
+        const data = await response.json()
+        console.log(data)
+        //procesamento
+    } catch (error) {
+        console.error('Erro')
+    }
 }
 
 getAddress("52091041")
+console.log('restante do codigo')
